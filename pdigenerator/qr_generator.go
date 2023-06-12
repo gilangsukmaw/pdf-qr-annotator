5b3f1b6dbda29dccc4db9dfef1a776d1c363bc43
GJ2345 <gilang.sukma@privyid.tech>
2023-06-12 15:47:35 +0700

    init

---
+++ pdigenerator/qr_generator.go
@@ -0,0 +1,39 @@
+package generate
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+	"io"
+)
+
+func GenerateQR(uuid string) ([]byte, error) {
+
+	qrc, err := qrcode.New(fmt.Sprintf(`https://privy.id/verify/%s`, uuid))
+	// qrc, err := qrcode.New("with-custom-shape", qrcode.WithCircleShape())
+	if err != nil {
+		return nil, err
+	}
+
+	// get bytes
+	buf := bytes.NewBuffer(nil)
+	wr := nopCloser{Writer: buf}
+	w2 := standard.NewWithWriter(
+		wr,
+		standard.WithQRWidth(5),
+		standard.WithBorderWidth(1),
+		standard.WithLogoImageFilePNG("./logo/logo_25.png"),
+	)
+	if err = qrc.Save(w2); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type nopCloser struct {
+	io.Writer
+}
+
+func (nopCloser) Close() error { return nil }
